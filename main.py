ee9a63544e2e54849d16c86035dee409a4994e7c
SMC-Youri <95485150+SMC-Youri@users.noreply.github.com>
2023-09-25 09:27:42 +0000

    Grafiek naar cirkeldiagram veranderd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
+from bke import MLAgent, is_winner, opponent, RandomAgent, validate, plot_validation
  
 class MyAgent(MLAgent):
     def evaluate(self, board):
@@ -16,9 +16,9 @@ random.seed(1)
 my_agent = MyAgent()
 random_agent = RandomAgent()
  
-train_and_plot(
-    agent=my_agent,
-    validation_agent=random_agent,
-    iterations=50,
-    trainings=100,
-    validations=1000)
+validation_result = validate(    
+    agent_x=my_agent,
+    agent_o=random_agent,
+    iterations=50,)
+ 
+plot_validation(validation_result)
