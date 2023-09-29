a2f75db3722645b080bfc9c0bd43c5b6aa0e495d
SMC-Youri <95485150+SMC-Youri@users.noreply.github.com>
2023-09-29 10:45:08 +0000

    Manier van trainen aangepast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from bke import MLAgent, is_winner, opponent, RandomAgent, validate, plot_validation, train
+from bke import MLAgent, is_winner, opponent, RandomAgent, validate, plot_validation, train, train_and_plot, train_and_validate
  
 class MyAgent(MLAgent):
     def evaluate(self, board):
@@ -13,14 +13,21 @@ class MyAgent(MLAgent):
  
 random.seed(1)
  
-my_agent = MyAgent(alpha=0.9, epsilon=0.2)
+my_agent = MyAgent(alpha=0.1, epsilon=0.2)
 random_agent = RandomAgent()
 
-train(my_agent, 3000)
+train_and_validate(
+  agent=my_agent,
+  validation_agent=random_agent,
+  iterations=50,
+  trainings=500,
+  validations=1000)
+
+my_agent.learning = False
 
 validation_result = validate(    
     agent_x=my_agent,
     agent_o=random_agent,
-    iterations=50,)
+    iterations=10000,)
  
 plot_validation(validation_result)
